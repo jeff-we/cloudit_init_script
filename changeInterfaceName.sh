d7a58210be682f0c614a5182170d8f2b796fb926
orgensthe <w4102945@gmail.com>
2020-01-07 18:39:50 +0900

    complete changeInterfaceName.sh

--- changeInterfaceName.sh
+++ changeInterfaceName.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-echo "hello"
+sed -i '/GRUB_CMDLINE_LINUX/d' test.txt
+
+echo 'GRUB_CMDLINE_LINUX=" crashkernel=auto rd.lvm.lv=centos/root rd.lvm.lv=centos/swap rhgb quiet net.ifnames=0"' >> test.txt
+
+grub2-mkconfig -o /boot/grub2/grub.cfg
+
